0796e0686b76ed0b7221643f322d2ad1c9708d8e
Ashish <aashishkc98@gmail.com>
2019-10-05 18:06:10 -0700

    Added pod

---
+++ CircleTabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CircleTabBar
+//
+//  Created by Ashish on 10/5/19.
+//  Copyright © 2019 Greenhouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CircleTabBar/Pods-CircleTabBar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CircleTabBar : NSObject
+@end
+@implementation PodsDummy_Pods_CircleTabBar
+@end
